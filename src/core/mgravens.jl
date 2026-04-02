cbc4d0eb7870ceb7ab0c3c875d025c911fd372b3
wbecker <william.becker@nrel.gov>
2026-04-01 18:58:11 -0600

    Handle existing BESS after Ravens defaults update
    These BESS cost values are not included in the Ravens defaults anymore, so need to hard code them when subtracting out cost of existing BESS capacity.

--- src/core/mgravens.jl
+++ src/core/mgravens.jl
@@ -602,9 +602,9 @@ function convert_mgravens_inputs_to_reopt_inputs(mgravens::Dict)
                 lumped_const_power_fraction = 0.5
                 lumped_const_ref_kw = 1800.0
                 lumped_const_ref_kwh = 7200.0
-                installed_cost_per_kw = copy(reopt_inputs["ElectricStorage"]["installed_cost_per_kw"])
-                installed_cost_per_kwh = copy(reopt_inputs["ElectricStorage"]["installed_cost_per_kwh"])
-                installed_cost_constant = copy(reopt_inputs["ElectricStorage"]["installed_cost_constant"])
+                installed_cost_per_kw = 968.0 #copy(reopt_inputs["ElectricStorage"]["installed_cost_per_kw"])
+                installed_cost_per_kwh = 253.0 #copy(reopt_inputs["ElectricStorage"]["installed_cost_per_kwh"])
+                installed_cost_constant = 222115.0 #copy(reopt_inputs["ElectricStorage"]["installed_cost_constant"])
                 reopt_inputs["ElectricStorage"]["installed_cost_per_kw"] = (installed_cost_per_kw * lumped_const_ref_kw + lumped_const_power_fraction * installed_cost_constant) / lumped_const_ref_kw
                 reopt_inputs["ElectricStorage"]["installed_cost_per_kwh"] = (installed_cost_per_kwh * lumped_const_ref_kwh + lumped_const_power_fraction * installed_cost_constant) / lumped_const_ref_kwh
                 reopt_inputs["ElectricStorage"]["installed_cost_constant"] = -cost(existing_battery_kw, existing_battery_kwh)
