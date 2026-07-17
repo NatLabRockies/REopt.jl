8a8a83538d32a1f887f31e7b1e4f42c0e5943b61
adfarth <adfarthing1@gmail.com>
2026-07-17 15:33:33 -0700

    fix bug with mthly ts and mpc

--- src/constraints/electric_utility_constraints.jl
+++ src/constraints/electric_utility_constraints.jl
@@ -26,6 +26,13 @@ function add_export_constraints(m, p; _n="")
     NEM_storage = String[b for b in p.s.storage.types.elec if :NEM in p.export_bins_by_storage[b]]
     WHL_storage = String[b for b in p.s.storage.types.elec if :WHL in p.export_bins_by_storage[b]]
 
+    # TEMP
+    println("NEM_techs: ", NEM_techs)
+    println("WHL_techs: ", WHL_techs)
+    println("interconnection_limit: ", p.s.electric_utility.interconnection_limit_kw)
+    println("NEM_storage: ", NEM_storage)
+    println("WHL_storage: ", WHL_storage)
+
     if !isempty(vcat(NEM_techs, NEM_storage))
         # Constraint (9c): Net metering only -- can't sell more than you purchase
         # hours_per_time_step is cancelled on both sides, but used for unit consistency (convert power to energy)
@@ -91,7 +98,7 @@ function add_export_constraints(m, p; _n="")
                         p.s.electric_load.loads_kw[ts] + 
                         p.s.cooling_load.loads_kw_thermal[ts]/p.cooling_cop["ExistingChiller"][ts] + 
                         (p.s.space_heating_load.loads_kw[ts] + p.s.dhw_load.loads_kw[ts] + p.s.process_heat_load.loads_kw[ts]) 
-                    ) for ts in p.s.electric_tariff.time_steps_monthly[m]) for m in p.months
+                    ) for ts in p.s.electric_tariff.time_steps_monthly[m]; init=0.0) for m in p.months
                     ])
                 ])
                 
@@ -311,7 +318,7 @@ function add_tou_peak_constraint(m, p; _n="")
             b[r, tier] <= b[r, tier-1]
         )
 
-        # One ratchet peak electrical power demand tier must be full before next one is active
+        # Within each ratchet, one tier's peak electrical power demand tier must be full before next one is active
         @constraint(m, [r in p.ratchets, tier in 2:ntiers],
             b[r, tier] * bigM_tou_demand_tier_limits[r, tier-1] 
             <= m[Symbol("dvPeakDemandTOU"*_n)][r, tier-1]
