42c414361159cfb1c09f862a0c5444ab8157b122
adfarth <adfarthing1@gmail.com>
2026-08-07 12:33:09 -0600

    fix mpc storage to load

--- src/results/electric_storage.jl
+++ src/results/electric_storage.jl
@@ -93,15 +93,18 @@ function add_electric_storage_results(m::JuMP.AbstractModel, p::MPCInputs, d::Di
     soc = (m[Symbol("dvStoredEnergy"*_n)][b, ts] for ts in p.time_steps)
     r["soc_series_fraction"] = round.(value.(soc) ./ p.s.storage.attr[b].size_kwh, digits=3)
 
-    discharge = (m[Symbol("dvDischargeFromStorage"*_n)][b, ts] for ts in p.time_steps)
-    r["storage_to_load_series_kw"] = round.(value.(discharge), digits=3)
-
     r["storage_to_grid_series_kw"] = zeros(size(r["soc_series_fraction"]))
     if !isempty(p.s.electric_tariff.export_bins)
         StorageToGrid = @expression(m, [ts in p.time_steps],
             sum(m[Symbol("dvStorageToGrid"*_n)][b, u, ts] for u in p.export_bins_by_storage[b]))
         r["storage_to_grid_series_kw"] = results_array(round.(value.(StorageToGrid), digits=3))
     end
+
+    r["storage_to_load_series_kw"] = zeros(size(r["soc_series_fraction"]))
+    StorageToLoad = ( m[Symbol("dvDischargeFromStorage"*_n)][b, ts] 
+                        - r["storage_to_grid_series_kw"][ts] for ts in p.time_steps
+    )
+    r["storage_to_load_series_kw"] = round.(value.(StorageToLoad), digits=3)
 
     d[b] = r
     nothing
